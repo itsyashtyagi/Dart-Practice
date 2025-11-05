62b209a3df6e0ff15efc417264784f4219e2a87f
yashmobrilz <yash@mobrilz.agency>
2025-11-05 18:29:23 +0530

    Sets code

--- practice/sets.dart
+++ practice/sets.dart
@@ -1,5 +1,15 @@
 void main() {
   const a = <int>{1, 3};
   const b = <int>{3, 5};
-  const c = {};
+  final c = a.union(b).difference(a.intersection(b));
+
+  print(a.union(b));
+  print(a.intersection(b));
+  print(c);
+
+  var sum = 0;
+  for (var val in c) {
+    sum += val;
+  }
+  print(sum);
 }
